cf5fac10947f89f09559725bb878c42c06854a65
huynguyenhuu <huy.nguyenhuu.017@gmail.com>
2023-04-16 19:09:19 +0700

    push

--- crawler.py
+++ crawler.py
@@ -9,35 +9,32 @@ from time import sleep
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
-from tqdm import tqdm
+#from tqdm import tqdm
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 # Khởi tạo driver
-driver = webdriver.Chrome(executable_path="chromedriver_win32/chromedriver.exe", options=chrome_options)
+driver = webdriver.Chrome(executable_path="C:/Users/huyng/Downloads/chromedriver_win32/chromedriver.exe", options=chrome_options)
 driver.get("https://vnexpress.net/")
+# Tìm các bài báo
+posts = driver.find_elements_by_class_name("title-news")
 
 # Khai báo một danh sách lưu trữ dữ liệu thu thập được
 data = []
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-links = []
-for link in soup.find_all('a'):
-    href = link.get('href')
-    if href.startswith('https://vnexpress.net/') and href.endswith('.html'):
-        links.append(href)
 # Lặp qua từng bài viết
-for link in links:
-    driver.get(link)
-    soup_page = BeautifulSoup(driver.page_source, 'html.parser')
+for post in posts:
     # Lấy tiêu đề của bài viết
-    if soup_page.find('h1', "title-detail") != None:
-        continue
-    title = soup_page.find('h1', "title-detail").text
+    title = post.text
+    # Click vào bài viết để xem chi tiết
+    post.click()
+    # Đợi load page
+    driver.implicitly_wait(10)
+
     content = driver.find_element_by_class_name("fck_detail").text
     # Lấy comment
-    comments = soup.find_all('div', "content-comment")
+    comments = driver.find_element_by_class_name("content-comment")
     comment_list = []
     for comment in comments:
         text = comment.find_element_by_class_name("full_content").text
@@ -50,8 +47,8 @@ for link in links:
         # Lưu thông tin của bài viết và bình luận vào danh sách
         data.append({"title": title, "content": content, "comments": comment_list})
         # Quay trở lại trang chủ
-        driver.quit()
-print(data)
+        driver.back()
+driver.quit()
 df = pd.DataFrame(data)
 
 # Xuất dữ liệu thành file CSV
